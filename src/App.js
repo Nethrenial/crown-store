d4ad5b6201b37cac490e3f9ca5056a548a40c50a
Nethsara Sandeepa <kenwyedward@gmail.com>
2020-11-07 22:56:53 +0530

    implemented collection-previews

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shoppage/shop.component";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={Shop} />
     </div>
   );
 }
